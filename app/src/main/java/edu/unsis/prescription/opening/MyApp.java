89508b8e376d6e7dc6648cf2df17e969dfa0b0c2
VicenteMdz <mebvicente@gmail.com>
2017-10-28 19:12:14 -0500

    agrego pantalla para la carga inicial de datos

--- app/src/main/java/edu/unsis/prescription/opening/MyApp.java
+++ app/src/main/java/edu/unsis/prescription/opening/MyApp.java
@@ -5,6 +5,7 @@ import android.os.SystemClock;
 
 /**
  * Created by Meltsan on 28/10/17.
+ * se ejecuta al abrir la aplicacion y hace un delay de 3 seg. mientras se muestra la ventana splash
  */
 public class MyApp extends Application {
 
--- app/src/main/java/edu/unsis/prescription/opening/loading_app.java
+++ app/src/main/java/edu/unsis/prescription/opening/loading_app.java
@@ -4,14 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import edu.unsis.prescription.MainActivity;
+import edu.unsis.prescription.RegisterActivity;
 
+/**
+ * @Author: Vicente Mendoza
+ * @date: 28/10/2017
+ * Clase que se ejecuta con el activity splash al abrir la aplicacion
+ */
 public class loading_app extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
+        //registramos el siguiente activity a mostrar
+        Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
         finish();
     }
