6bad76c4d6c8a9a85563cedc81f31a19a17212f1
VicenteMdz <mebvicente@gmail.com>
2017-10-28 20:22:57 -0500

    add fragments navigations

--- app/src/main/java/edu/unsis/prescription/opening/loading_app.java
+++ app/src/main/java/edu/unsis/prescription/opening/loading_app.java
@@ -4,14 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import edu.unsis.prescription.RegisterActivity;
+import edu.unsis.prescription.registration.RegisterActivity;
 
 /**
  * @Author: Vicente Mendoza
  * @date: 28/10/2017
  * Clase que se ejecuta con el activity splash al abrir la aplicacion
  */
-public class loading_app extends AppCompatActivity {
+public class Loading_app extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/edu/unsis/prescription/registration/RegisterActivity.java
+++ app/src/main/java/edu/unsis/prescription/registration/RegisterActivity.java
@@ -1,9 +1,15 @@
-package edu.unsis.prescription;
+package edu.unsis.prescription.registration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import edu.unsis.prescription.R;
+import edu.unsis.prescription.home.Home;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -24,5 +30,22 @@ public class RegisterActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_register, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.saveUser:
+                saveUser();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void saveUser(){
+        Intent newAct = new Intent(getApplicationContext(), Home.class);
+        startActivity(newAct);
     }
 }
